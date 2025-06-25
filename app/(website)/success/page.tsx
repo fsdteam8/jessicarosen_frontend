3aa8233dd27b20ed030b0abb993c2b4f02fedaa1
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-06-25 12:44:09 +0600

    download page and happy customer section add

--- app/(website)/success/page.tsx
+++ app/(website)/success/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import OtherHistory from "./_components/OtherHistory";
 import HappyCustomer from "./_components/HappyCustomer";
+import LegalDoc from "@/components/HomePage/LegalDoc";
 
 export default function page() {
   return (
     <div className="">
       <OtherHistory />
       <HappyCustomer />
+      <LegalDoc />
     </div>
   );
 }
