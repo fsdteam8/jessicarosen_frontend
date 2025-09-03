726baa32cb195466762620261167dc6c86dc588d
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-09-03 10:38:00 +0600

    optional chaning use

--- app/(website)/store/[slug]/page.tsx
+++ app/(website)/store/[slug]/page.tsx
@@ -4,7 +4,7 @@ import Products from '../../products/page'
 const Page = ({params}:{params:{slug:string}}) => {
   return (
     <div className='container'>
-    <h2 className='text-3xl font-bold text-black leading-normal pt-10'>{decodeURIComponent(params.slug)}</h2>
+    <h2 className='text-3xl font-bold text-black leading-normal pt-10'>{decodeURIComponent(params?.slug)}</h2>
 
       <Products/>
     </div>
